193162e06820a6ba491d3bb44074a0eb6f40f13c
Dandy Raka <dandyraka13@gmail.com>
2025-06-29 01:40:52 +0700

    Update generate-readme.js

--- generate-readme.js
+++ generate-readme.js
@@ -2,16 +2,16 @@ import fs from "fs";
 
 const history = fs.readFileSync("history.txt", "utf-8").trim().split("\n");
 
-const tableRows = history.map(line => {
+const tableRows = history.map((line, index) => {
     const [member, judul, tanggal, cover] = line.split(" | ");
     const imageMarkdown = `<img src="${cover}" alt="${judul}" width="200"/>`; // ukuran medium (200px)
-    return `| ${member} | ${judul} | ${tanggal} | ${imageMarkdown} |`;
+    return `| ${index + 1} | ${member} | ${judul} | ${tanggal} | ${imageMarkdown} |`;
 });
 
 const markdownTable = `# History IDN Live JKT48
 
-| Member | Judul | Tanggal | Cover |
-|--------|-------|---------|-------|
+| No | Member | Judul | Tanggal | Cover |
+|----|--------|-------|---------|-------|
 ${tableRows.join("\n")}
 `;
 
